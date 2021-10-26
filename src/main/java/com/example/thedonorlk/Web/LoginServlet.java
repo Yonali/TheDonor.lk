afee06987138cb38d067dff1f9234968b1c885ca
Akthar Farvees <aktharfarveess@gmail.com>
2021-10-26 19:35:16 +0530

    Bug Fixes

--- src/main/java/com/example/thedonorlk/Web/LoginServlet.java
+++ src/main/java/com/example/thedonorlk/Web/LoginServlet.java
@@ -5,12 +5,14 @@ import com.example.thedonorlk.Database.LoginDAO;
 import org.apache.commons.codec.digest.DigestUtils;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
