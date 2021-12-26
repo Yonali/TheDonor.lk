285867d8c4bff3bb9ff611953dfb04ed25b3eb05
Akthar Farvees <aktharfarveess@gmail.com>
2021-12-26 23:30:48 +0530

    Bug fixes

--- src/main/webapp/public/scripts/cardview_toggle.js
+++
@@ -1,8 +0,0 @@
-$(window).load(function() {
-    $('.post-module').hover(function() {
-      $(this).find('.description').stop().animate({
-        height: "toggle",
-        opacity: "toggle"
-      }, 300);
-    });
-  });
--- src/main/webapp/public/scripts/scripts.js
+++
@@ -1,6 +0,0 @@
-function loadIframe() {
-    // this function will load the Google homepage into the iframe
-    var iframe = document.getElementById("icontent");
-    iframe.src = "http://localhost:8080/TheDonorlk_war_exploded/view/non_donor/dashboard.jsp" // here goes your url
-    };
-    window.onload = loadIframe;
